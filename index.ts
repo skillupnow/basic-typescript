c94fe190b1e5d11c397d52a05a39c65a61c7dc74
Andrei Miulescu <lusu777@gmail.com>
2020-03-09 10:59:53 +1100

    Lesson 9 - Going to the zoo

--- index.ts
+++ index.ts
@@ -14,6 +14,10 @@ abstract class Animal {
     return this.species;
   }
 
+  public feed() {
+    return `${this.speak()} I am full`;
+  }
+
   public abstract speak(): string;
 }
 
@@ -37,10 +41,38 @@ class Dog extends Animal {
   }
 }
 
-class Visitor {
+interface Person {
+  name: string;
+  visit: (animal: Animal) => void;
+}
+
+interface AnimalFeeder {
+  feeds(animal: Animal): void;
+}
+
+class Visitor implements Person {
   name: string;
   constructor(name: string) {
     this.name = name;
+  }
+
+  public visit(animal: Animal) {
+    return animal.speak();
+  }
+}
+
+class Staff implements Person, AnimalFeeder {
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  public feeds(animal: Animal) {
+    return animal.feed();
+  }
+
+  public visit(animal: Animal) {
+    console.log(animal.speak());
   }
 }
 
@@ -48,15 +80,42 @@ interface Zoo<Occupants> {
   occupants: Array<Occupants>;
 }
 
-type ZooOccupants = Animal | Visitor;
+type ZooOccupants = Animal | Visitor | Staff;
 
 const ourZoo: Zoo<ZooOccupants> = {
-  occupants: [new Cat(), new Dog(), new Visitor("Mike")]
+  occupants: [
+    new Cat(),
+    new Dog(),
+    new Visitor("Mike"),
+    new Staff("Zoo Keeper")
+  ]
 };
+
+function isAnimal(occupant: ZooOccupants): occupant is Animal {
+  return "speak" in occupant;
+}
+function isStaff(occupant: ZooOccupants): occupant is Staff {
+  return "feeds" in occupant;
+}
 
 function helloWorld(world: Planet, zoo: Zoo<ZooOccupants>): string {
   let output: string = `Hello, ${world}`;
   output = `${output}. Our zoo has ${zoo.occupants.length} occupants in it.`;
+  for (const occupant of zoo.occupants) {
+    if (isAnimal(occupant)) {
+      console.log(occupant.speak());
+    }
+    if (!isAnimal(occupant)) {
+      const firstAnimal = zoo.occupants[0];
+      if (isAnimal(firstAnimal)) {
+        if (isStaff(occupant)) {
+          console.log("Feed", occupant.feeds(firstAnimal));
+        } else {
+          console.log("Visit", occupant.visit(firstAnimal));
+        }
+      }
+    }
+  }
   return output;
 }
 
