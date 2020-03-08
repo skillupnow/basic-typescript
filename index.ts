85ad2236cffac9d736976c4db73e5e32c4311ba1
Andrei Miulescu <lusu777@gmail.com>
2020-03-08 14:38:11 +1100

    Lesson 5 - Generics

--- index.ts
+++ index.ts
@@ -4,24 +4,59 @@ enum Planet {
   Red = "Mars"
 }
 
-class Animal {
+abstract class Animal {
   public species: string;
+  constructor(species: string) {
+    this.species = species;
+  }
+
+  public whatAmI(): string {
+    return this.species;
+  }
+
+  public abstract speak(): string;
+}
+
+class Cat extends Animal {
   constructor() {
-    this.species = "cat";
+    super("cat");
+  }
+
+  public speak() {
+    return "meow";
   }
 }
 
-interface Zoo {
-  animals: Array<Animal>;
+class Dog extends Animal {
+  constructor() {
+    super("dog");
+  }
+
+  public speak() {
+    return "woof";
+  }
 }
 
-const ourZoo: Zoo = {
-  animals: [new Animal()]
+class Visitor {
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+interface Zoo<Occupants> {
+  occupants: Array<Occupants>;
+}
+
+type ZooOccupants = Animal | Visitor;
+
+const ourZoo: Zoo<ZooOccupants> = {
+  occupants: [new Cat(), new Dog(), new Visitor("Mike")]
 };
 
-function helloWorld(world: Planet, zoo: Zoo): string {
+function helloWorld(world: Planet, zoo: Zoo<ZooOccupants>): string {
   let output: string = `Hello, ${world}`;
-  output = `${output}. Our zoo has ${zoo.animals.length} animals in it.`;
+  output = `${output}. Our zoo has ${zoo.occupants.length} occupants in it.`;
   return output;
 }
 
