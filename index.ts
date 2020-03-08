33698757a6f2706f218c7753ad33382458aba796
Andrei Miulescu <lusu777@gmail.com>
2020-03-08 14:14:56 +1100

    Lesson 3 - Variables and basic types

--- index.ts
+++ index.ts
@@ -1,2 +1,15 @@
-const output: string = "Hello, world";
-console.log(output);
+enum Planet {
+  Green = "Greenland",
+  Blue = "Earth",
+  Red = "Mars"
+}
+
+function helloWorld(world: Planet, verbose?: boolean): string {
+  let output: string = `Hello, ${world}`;
+  if (verbose) {
+    output = `Hello, fellows from planet ${world}.`;
+  }
+  return output;
+}
+
+console.log(helloWorld(Planet.Blue));
